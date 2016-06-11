7a7fb2f1a07866350fb60d17293db69f72645639
vivalaralstin <dan@rYOsTQKB19.local>
2016-06-10 17:11:26 -0700

    syntax and table name changes

--- tableau.virtual_economy_sources.sql
+++ tableau.virtual_economy_sources.sql
@@ -11,40 +11,40 @@ zz.defender_ext_multi::VARCHAR(1024) as defender_ext_multi,
 zzz.defender_ext_extra::VARCHAR(1024) as defender_ext_extra,
 zx.defender_ext_wmo::VARCHAR(1024) as defender_ext_wmo,
   
-CASE WHEN x.source::VARCHAR(1024) = 'battle' 
-	AND zzz.defender_ext_extra::VARCHAR(1024) is null 
-	AND zz.defender_ext_multi::VARCHAR(1024) is not null 
-	AND zx.defender_ext_wmo::VARCHAR(1024) is null
-		   THEN zz.defender_ext_multi::VARCHAR(1024)
+	   CASE WHEN x.source::VARCHAR(1024) = 'battle' 
+			AND zzz.defender_ext_extra::VARCHAR(1024) is null 
+			 AND zz.defender_ext_multi::VARCHAR(1024) is not null 
+			  AND zx.defender_ext_wmo::VARCHAR(1024) is null
+				   THEN zz.defender_ext_multi::VARCHAR(1024)
 
-	WHEN x.source::VARCHAR(1024) = 'battle' 
-	AND zzz.defender_ext_extra::VARCHAR(1024) is null 
-	AND zz.defender_ext_multi::VARCHAR(1024) is null 
-	AND zx.defender_ext_wmo::VARCHAR(1024) is not null
-	       THEN zx.defender_ext_wmo::VARCHAR(1024)	
+			WHEN x.source::VARCHAR(1024) = 'battle' 
+			AND zzz.defender_ext_extra::VARCHAR(1024) is null 
+			 AND zz.defender_ext_multi::VARCHAR(1024) is null 
+			  AND zx.defender_ext_wmo::VARCHAR(1024) is not null
+			       THEN zx.defender_ext_wmo::VARCHAR(1024)	
 
-	WHEN x.source::VARCHAR(1024) = 'battle' 
-	AND zzz.defender_ext_extra::VARCHAR(1024) is null 
-	AND zz.defender_ext_multi::VARCHAR(1024) is not null 
-	AND zx.defender_ext_wmo::VARCHAR(1024) is not null
-	       THEN zx.defender_ext_wmo::VARCHAR(1024)	
+			WHEN x.source::VARCHAR(1024) = 'battle' 
+			AND zzz.defender_ext_extra::VARCHAR(1024) is null 
+			 AND zz.defender_ext_multi::VARCHAR(1024) is not null 
+			  AND zx.defender_ext_wmo::VARCHAR(1024) is not null
+			       THEN zx.defender_ext_wmo::VARCHAR(1024)	
 
-	WHEN x.source::VARCHAR(1024) = 'battle' 
-	AND zzz.defender_ext_extra::VARCHAR(1024) is not null 
-		   THEN zzz.defender_ext_extra::VARCHAR(1024)
+			WHEN x.source::VARCHAR(1024) = 'battle' 
+			AND zzz.defender_ext_extra::VARCHAR(1024) is not null 
+				   THEN zzz.defender_ext_extra::VARCHAR(1024)
 
-	WHEN x.source::VARCHAR(1024) = 'combat_victory'
-		   THEN 'event'
-	WHEN x.source::VARCHAR(1024) = 'event_reward'  
-		   THEN 'event'
-	WHEN x.source::VARCHAR(1024) in ('x1','x2','x3')
-		   THEN 'event'
-	WHEN x.source::VARCHAR(1024) LIKE('x4')
-		   THEN 'event' 
-	WHEN x.source::VARCHAR(1024) LIKE('x5')
-		   THEN 'forsaken_mission'  
-	ELSE x.source::VARCHAR(1024) 
-		   END as macro_source  
+			WHEN x.source::VARCHAR(1024) = 'combat_victory'
+				   THEN 'event'
+			WHEN x.source::VARCHAR(1024) = 'event_reward'  
+				   THEN 'event'
+			WHEN x.source::VARCHAR(1024) in ('x1','x2','x3')
+				   THEN 'event'
+			WHEN x.source::VARCHAR(1024) LIKE('x4')
+				   THEN 'event' 
+			WHEN x.source::VARCHAR(1024) LIKE('x5')
+				   THEN 'forsaken_mission'  
+			ELSE x.source::VARCHAR(1024) 
+				   END as macro_source  
    			   
 FROM (
 	SELECT distinct
@@ -56,12 +56,12 @@ FROM (
 	a.source,
 	a.amount
 		 
-	FROM dbtable2.user_currencies_transactions a
+	FROM db.user_currencies_transactions a
 	WHERE amount > 0
 	AND currencyid = 1
 	AND source != 'cs'
 	AND source != 'admin'
-	AND userid not in (SELECT distinct userid FROM bp.agg.employee_whitelist)
+	AND userid not in (SELECT distinct userid FROM db.whitelist)
 	AND TO_CHAR(CONVERT_TIMEZONE('UTC','America/Los_Angeles', a.addtime::timestamp_ntz), 'YYYY-MM-DD')  = '%s'
 	 ) x
 
@@ -72,7 +72,7 @@ FROM (
 				b.level as l, 
 				b.rev_fb_primary as ltv
 
-				FROM bp.public.users b
+				FROM db.users b
 				) y ON x.u::VARCHAR(1024) = y.u::VARCHAR(1024) 
  
 			LEFT OUTER JOIN (
@@ -83,7 +83,7 @@ FROM (
 				f.value:userid as u,
 				max(d.src:defender_ext) as defender_ext_multi
 
-				FROM bp.public.raw_para_json d, LATERAL FLATTEN(input => parse_json(d.src:attacker_level)) f
+				FROM db.raw_json d, LATERAL FLATTEN(input => parse_json(d.src:attacker_level)) f
 				WHERE d.tag = 'attack_multi'
 				AND d.dt_pst  = '%s'
 				GROUP BY d.src:battleid, f.value:userid
@@ -98,7 +98,7 @@ FROM (
 				dd.game_user_id as u,
 				dd.src:defending_fleetid as defender_ext_extra
 
-				FROM dbtable3.meta dd
+				FROM db.raw_json dd
 				WHERE dd.tag = 'attack_extra'
 				AND dt_pst = '%s'
 				) zzz ON x.battleid::VARCHAR(1024) = zzz.battleid::VARCHAR(1024) 
@@ -110,5 +110,5 @@ FROM (
 			    e.wmid as target_id,
 			    e.defender_ext as defender_ext_wmo
 			   
-			    FROM dbtable4.ref e
+			    FROM db.ref e
 			    ) zx ON x.target_id::VARCHAR(1024) = zx.target_id::VARCHAR(1024)
